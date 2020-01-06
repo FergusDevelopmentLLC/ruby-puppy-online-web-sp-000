2ce2080035ec22ca60c439a6ab2aa288c18a9c5b
Will Carter <will.carter@fergusllc.com>
2020-01-06 23:44:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,9 +28,31 @@ class Dog
   end
 
 end
-Dog.new("Pluto")
-Dog.new("Fido")
-Dog.new("Maddy")
-Dog.print_all
-Dog.clear_all
-Dog.print_all
+# Dog.new("Pluto")
+# Dog.new("Fido")
+# Dog.new("Maddy")
+# Dog.print_all
+# Dog.clear_all
+# Dog.print_all
+
+class Person
+  attr_accessor :name
+  @@people = []
+
+  def initialize(name)
+    @name = name
+    # self in the initialize method is our new instance
+    # self.class is Person
+    # self.class.all == Person.all
+    self.class.all << self
+  end
+
+  def self.all
+    @@people
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|person| person.name == name}
+  end
+
+end
